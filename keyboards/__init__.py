f131d885381a6e5b18c9a38590c81e2b3126c233
тnihosprop <niwibonihs@tutamail.com>
2025-01-19 13:41:31 +0300

    chore

---
+++ keyboards/__init__.py
@@ -0,0 +1,4 @@
+from .keyboards import *
+from .kb_utils import *
+from .set_menu import *
+from .buttons import *
---
+++ main.py
@@ -0,0 +1,74 @@
+import asyncio
+import logging
+from logging.config import dictConfig
+
+import yaml
+
+from aiogram.fsm.storage.redis import Redis, RedisStorage
+from aiogram import Bot, Dispatcher
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
+
+from config_data.config import Config, load_config
+from keyboards.set_menu import set_main_menu
+from handlers import admin_handlers, other_handlers, user_handlers
+from middlewares.outer import ThrottlingMiddleware
+
+logger_main = logging.getLogger(__name__)
+
+async def main():
+    with open('logs/logging_setting/log_config.yaml', 'rt') as file:
+        log_config = yaml.safe_load(file.read())
+    dictConfig(log_config)
+    logger_main.info('Loading logging settings success')
+
+    config: Config = load_config()
+    bot = Bot(token=config.tg_bot.token,
+              default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+
+    redis = Redis(host=config.redis_host, port=6379, db=0)
+
+    try:
+        await redis.ping()
+        logger_main.info("Redis connection established successfully.")
+    except Exception as err:
+        logger_main.error(f"Error connecting to Redis: {err}")
+        raise
+
+    storage = RedisStorage(redis=redis)
+    dp = Dispatcher(storage=storage)
+
+    # throttling storage
+    storage_throttling = RedisStorage.from_url(f'redis://{config.redis_host}:6379/7')
+
+    try:
+        logger_main.info('Loading from a db.json success')
+
+        await set_main_menu(bot)
+
+        # routers
+        dp.include_router(admin_handlers.admin_router)
+        dp.include_router(user_handlers.user_router)
+        dp.include_router(other_handlers.other_router)
+
+        # middlewares
+        dp.message.outer_middleware(
+                ThrottlingMiddleware(storage=storage_throttling, ttl=700))
+        dp.callback_query.outer_middleware(
+                ThrottlingMiddleware(storage=storage_throttling, ttl=500))
+
+        await bot.delete_webhook(drop_pending_updates=True)
+        logger_main.info('Start bot')
+        await dp.start_polling(bot, superadmin=config.tg_bot.id_admin)
+    except Exception as err:
+        logger_main.exception(err)
+        raise
+
+    finally:
+        await redis.aclose()
+        await storage_throttling.redis.aclose()
+        logger_main.info('Stop bot')
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
