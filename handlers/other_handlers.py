63f6cbddb2ca015e39872eea1463d54c294a4230
тnihosprop <niwibonihs@tutamail.com>
2025-01-19 17:31:46 +0300

    feat: update handlers
    Moved the msg_other handler from user_handlers.py to other_handlers.py
    Added text for the notification window for completing the survey

--- handlers/other_handlers.py
+++ handlers/other_handlers.py
@@ -1,6 +1,12 @@
 import logging
 
 from aiogram import F, Router
+from aiogram.types import Message
 
 other_router = Router()
 logger_user_hand = logging.getLogger(__name__)
+
+
+@other_router.message()
+async def msg_other(msg: Message):
+    await msg.delete()
--- handlers/user_handlers.py
+++ handlers/user_handlers.py
@@ -4,14 +4,14 @@ from aiogram import F, Router
 from aiogram.types import CallbackQuery, Message
 
 from lexicon.lexicon_ru import LexiconRu
-
+from keyboards.keyboards import kb_quiz
 user_router = Router()
 logger_user_hand = logging.getLogger(__name__)
 
 
 @user_router.message(F.text == '/start')
 async def cmd_start(msg: Message):
-    await msg.answer(LexiconRu.text_survey)
+    await msg.answer(LexiconRu.text_survey, reply_markup=kb_quiz)
 
 
 @user_router.callback_query(F.data == 'back')
