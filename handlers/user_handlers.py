41230bbe4e76e00834d73d0b8ea150f459398e99
тnihosprop <niwibonihs@tutamail.com>
2025-01-19 14:12:09 +0300

    refactor

--- handlers/user_handlers.py
+++ handlers/user_handlers.py
@@ -3,8 +3,6 @@ import logging
 from aiogram import F, Router
 from aiogram.types import CallbackQuery, Message
 
-from lexicon.lexicon_ru import LexiconRu
-from keyboards import kb_back
 
 user_router = Router()
 logger_user_hand = logging.getLogger(__name__)
@@ -12,11 +10,10 @@ logger_user_hand = logging.getLogger(__name__)
 
 @user_router.message(F.text == '/start')
 async def cmd_start(msg: Message):
-    await msg.answer('Меню', reply_markup=kb_menu)
+    await msg.answer('Анкета')
 
 
 @user_router.callback_query(F.data == 'back')
 async def clbk_back(clbk: CallbackQuery):
-    await clbk.message.edit_text(LexiconRu.text_word_guessed,
-                                 reply_markup=kb_game)
+    await clbk.message.edit_text('действие назад')
     await clbk.answer()
