8d35a62b1e07f43bf17570b1a78656be7ad16bef
тnihosprop <niwibonihs@tutamail.com>
2025-01-19 23:54:36 +0300

    feat: add keyboards

--- keyboards/keyboards.py
+++ keyboards/keyboards.py
@@ -1,5 +1,6 @@
 import logging
 
+from keyboards import BUTT_COURSES
 from keyboards.kb_utils import create_inline_kb
 from keyboards.buttons import ADMIN_PANEL_BUTT, BUTT_SEX
 
@@ -13,7 +14,9 @@ class KeyBoards:
         pass
 
 
-kb_admin = create_inline_kb(2, **ADMIN_PANEL_BUTT, cancel_butt=False)
-kb_quiz = create_inline_kb(start_quiz='Начать', cancel_butt=False)
-kb_select_sex = create_inline_kb(2, **BUTT_SEX, cancel_butt=False, back=True)
+kb_butt_quiz = create_inline_kb(start_quiz='Начать', cancel_butt=False)
 kb_butt_cancel = create_inline_kb(cancel_butt=True)
+
+kb_admin = create_inline_kb(2, **ADMIN_PANEL_BUTT, cancel_butt=False)
+kb_select_sex = create_inline_kb(2, **BUTT_SEX, back=True)
+kb_courses = create_inline_kb(**BUTT_COURSES, back=True)
