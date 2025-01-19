16aec1421f3f420cbe33aae62afd97b2162ebfb8
тnihosprop <niwibonihs@tutamail.com>
2025-01-19 13:17:20 +0300

    feat: add keyboards.py

---
+++ keyboards/keyboards.py
@@ -0,0 +1,18 @@
+import logging
+
+
+from keyboards.kb_utils import create_inline_kb
+from keyboards.buttons import ADMIN_PANEL_BUTT
+
+logger_keyboards = logging.getLogger(__name__)
+
+
+class KeyBoards:
+
+    @staticmethod
+    def kb_game():
+        pass
+
+
+kb_admin = create_inline_kb(2, **ADMIN_PANEL_BUTT, cancel_butt=False)
+kb_back = create_inline_kb(width=1, back='Назад', cancel_butt=False)
