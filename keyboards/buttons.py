8b286fcc072357a776b8d5848cdd4a413e2bb02c
тnihosprop <niwibonihs@tutamail.com>
2025-01-19 23:51:53 +0300

    feat: add button

--- keyboards/buttons.py
+++ keyboards/buttons.py
@@ -12,3 +12,9 @@ BUTT_CANCEL: dict[str, str] = {'cancel': '❌ОТМЕНА'}
 BUTT_BACK: dict[str, str] = {'back': 'Назад'}
 
 BUTT_SEX: dict[str, str] = {'male': 'Мужской', 'female': 'Женский'}
+BUTT_COURSES: dict[str, str] = {
+        'course_1': 'Лучший по Python. Часть 1',
+        'course_2': 'Лучший по Python. Часть 2',
+        'course_3': 'Лучший по Python. Часть 3',
+        'course_4': 'Лучший по Python. Часть 4',
+        'course_5': 'Лучший по Python. Часть 5'}
