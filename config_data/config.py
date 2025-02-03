4c637832e336cc4a0676e5f376df8a49dbf1761b
тnihosprop <niwibonihs@tutamail.com>
2025-02-03 19:38:40 +0300

    chore

--- config_data/config.py
+++ config_data/config.py
@@ -22,7 +22,6 @@ class Config:
     stepik: Stepik
     level_log: str
     w_text: bool
-    path_to_logs: str
 
 
 def load_config(path: str | None = None) -> Config:
@@ -33,11 +32,10 @@ def load_config(path: str | None = None) -> Config:
     stepik_client_cecret = env('STEPIK_CLIENT_CECRET')
     level_log = env('LOG_LEVEL', 'INFO')
     w_text = env('W_TEXT_ENABLED', False)
-    path_to_logs = env.str('PATH_TO_REC_LOGS')
     return Config(tg_bot=TgBot(token=env('BOT_TOKEN'),
                                id_admins=env('ID_ADMIN')),
                   stepik=Stepik(client_id=stepik_client_id,
                                 client_cecret=stepik_client_cecret),
                   redis_host=redis_host,
                   level_log=level_log,
-                  w_text=w_text, path_to_logs=path_to_logs)
+                  w_text=w_text)
