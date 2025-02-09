2f06560480adfb551aa42e27fec131971b8e744f
тnihosprop <niwibonihs@tutamail.com>
2025-02-09 21:35:15 +0300

    chore: update
    Some settings are made in functions: setup_logging and setup_redis

--- main.py
+++ main.py
@@ -11,54 +11,54 @@ from aiogram.enums import ParseMode
 from config_data.config import Config, load_config
 from keyboards.set_menu import set_main_menu
 from handlers import admin_handlers, other_handlers, user_handlers
-from middlewares.outer import (RedisMiddleware,
-                               ThrottlingMiddleware,
-                               MsgProcMiddleware)
+from middlewares.outer import (MsgProcMiddleware,
+                               RedisMiddleware,
+                               ThrottlingMiddleware)
 
 logger_main = logging.getLogger(__name__)
 
 
-async def main():
-
-    config: Config = load_config()
-
+async def setup_logging(config):
     with open('logs/logging_setting/log_config.yml', 'rt') as file:
         config_str = file.read()
     # вставляем(заменяем шаблоны на) переменные окружения
-    config_str = config_str.replace(
-            '${LOG_LEVEL}', config.level_log)
+    config_str = config_str.replace('${LOG_LEVEL}', config.level_log)
     log_config = yaml.safe_load(config_str)
     dictConfig(log_config)
-
     logger_main.info('Loading logging & config success')
-    bot = Bot(token=config.tg_bot.token,
-              default=DefaultBotProperties(parse_mode=ParseMode.HTML))
 
-    # Redis for FSM (db=0)
+
+async def setup_redis(config: Config) -> tuple[Redis, RedisStorage, Redis]:
+    """Настройка Redis для FSM, throttling и данных пользователей."""
     redis_fsm = Redis(host=config.redis_host, port=6379, db=0,
                       decode_responses=True)
-    storage = RedisStorage(redis=redis_fsm)
-
-    # Redis for throttling storage (db=1)
-    storage_throttling = RedisStorage.from_url(
+    redis_throttling = RedisStorage.from_url(
             f'redis://{config.redis_host}:6379/1')
-
-    # Redis for user data (db=2)
     redis_data = Redis(host=config.redis_host, port=6379, db=2,
                        decode_responses=True)
-
     try:
         await redis_fsm.ping()
         logger_main.info("Redis connection established successfully.")
     except Exception as err:
         logger_main.error(f"Error connecting to Redis: {err}")
         raise
+    return redis_fsm, redis_throttling, redis_data
 
+
+async def main():
+    config: Config = load_config()
+    await setup_logging(config)
+    bot = Bot(token=config.tg_bot.token,
+              default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+
+    # broadcast_redis = RedisSettings(host=config.redis_host, port=6379,
+    #                                 database=3)
+    redis_fsm, storage_throttling, redis_data = await setup_redis(config)
+    storage = RedisStorage(redis=redis_fsm)
     dp = Dispatcher(storage=storage)
+    await set_main_menu(bot)
 
     try:
-        await set_main_menu(bot)
-
         # routers
         dp.include_router(admin_handlers.admin_router)
         dp.include_router(user_handlers.user_router)
