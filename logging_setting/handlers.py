ab8572cc53fd7e3cf3f13838406a2e4d08009c2c
nihosprop <niwibonihs@tutamail.com>
2025-10-18 01:17:14 +0300

    feat: add TelegramAsyncHandler

---
+++ logging_setting/handlers.py
@@ -0,0 +1,50 @@
+import logging
+import asyncio
+import os
+import aiohttp
+from dotenv import load_dotenv
+
+logger = logging.getLogger(__name__)
+
+class TelegramAsyncHandler(logging.Handler):
+    def __init__(self, bot_token: str, chat_id: str, thread_id: str,
+                 loop=None):
+        
+        super().__init__()
+        
+        load_dotenv()
+        
+        self.api_url = f"https://api.telegram.org/bot{bot_token}/sendMessage"
+        self.chat_id = int(chat_id)
+        self.thread_id = int(thread_id)
+        self.session: aiohttp.ClientSession | None = None
+        self.loop = loop or asyncio.get_event_loop()
+        
+    async def _ensure_session(self):
+        if self.session is None or self.session.closed:
+            self.session = aiohttp.ClientSession()
+    
+    def emit(self, record):
+        log_entry = self.format(record)
+        self.loop.call_soon_threadsafe(asyncio.create_task,
+                                  self._send(text=log_entry))
+    
+    async def _send(self, text: str):
+        try:
+            await self._ensure_session()
+            async with self.session.post(
+                self.api_url,
+                data={
+                    "chat_id": self.chat_id,
+                    "message_thread_id": self.thread_id,
+                    "text": text}) as resp:
+                response = await resp.text()
+                if resp.status != 200:
+                    logger.error(f"Ошибка при отправке: {resp.status}"
+                                 f":{response}")
+        except Exception as e:
+            print(f"Ошибка отправки лога в Telegram: {e}")
+    
+    async def aclose(self):
+        if self.session and not self.session.closed:
+            await self.session.close()
