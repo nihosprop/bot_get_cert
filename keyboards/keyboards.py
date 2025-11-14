ffde95eee9175f6674f9e1bc3bd9654f452d9f12
nihosprop <niwibonihs@tutamail.com>
2025-11-14 17:01:37 +0300

    refactor

--- keyboards/keyboards.py
+++ keyboards/keyboards.py
@@ -14,9 +14,11 @@ class KeyBoards:
 kb_create_promo = create_inline_kb(
     width=1, url_buttons=BUTTS_URL_PROMO, cancel_butt=True,
     **BUTTS_PROMO_OTHER_AUTHOR)
+
 kb_butt_quiz = create_inline_kb(
     1, **BUTT_START,
     cancel_butt=False)
+
 kb_butt_cancel = create_inline_kb(cancel_butt=True)
 
 kb_admin = create_inline_kb(2, **ADMIN_PANEL_BUTT, cancel_butt=False)
