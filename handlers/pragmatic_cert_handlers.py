386643819d31e23a2ef192fb5281dfa95d7d6bdb
nihosprop <niwibonihs@tutamail.com>
2025-11-21 00:16:25 +0300

    feat(module): add and include pragmatic_cert_handlers module

---
+++ handlers/pragmatic_cert_handlers.py
@@ -0,0 +1,22 @@
+import logging
+
+from aiogram import Router
+from aiogram.filters import StateFilter, or_f
+from aiogram.fsm.context import FSMContext
+from aiogram.types import CallbackQuery
+
+from filters.filters import IsPragmaticCoursesFilter
+from utils import get_username
+
+router = Router()
+router.callback_query.filter(or_f(IsPragmaticCoursesFilter(), ))
+logger = logging.getLogger(__name__)
+
+
+@router.callback_query(IsPragmaticCoursesFilter())
+async def clbk_pragmatic_courses(clbk: CallbackQuery,
+                                 state: FSMContext):
+    await clbk.answer('Сертификат в разработке 🛠️', show_alert=True)
+    logger.warning(
+        f'Нажатие на курс {clbk.data}:{clbk.from_user.id}:'
+        f'{await get_username(clbk)}')
