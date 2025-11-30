e2e145253c5cd7b68d0c366c0ec4c711e66fb6d3
nihosprop <niwibonihs@tutamail.com>
2025-11-30 23:27:19 +0300

    feat: add handlers/common.py module

---
+++ handlers/common.py
@@ -0,0 +1,36 @@
+import logging
+
+from aiogram import Router, F
+from aiogram.fsm.context import FSMContext
+from aiogram.types import CallbackQuery
+
+from filters.filters import IsPrivateChat, IsAdmins
+from keyboards.keyboards import get_kb_courses, kb_butt_quiz
+from lexicon import LexiconRu
+from utils import MessageProcessor
+
+router = Router()
+router.message.filter(IsPrivateChat())
+logger = logging.getLogger(__name__)
+
+@router.callback_query(F.data == 'exit')
+async def clbk_exit(clbk: CallbackQuery,
+                   state: FSMContext,
+                   msg_processor: MessageProcessor):
+    logger.debug(f'Entry')
+
+    await msg_processor.deletes_messages(msgs_for_del=True)
+    try:
+        value = await clbk.message.answer(
+            LexiconRu.text_survey,
+            reply_markup=kb_butt_quiz,
+            disable_web_page_preview=True)
+    except Exception as err:
+        logger.error(f'{err.__class__.__name__}', exc_info=True)
+    else:
+        await msg_processor.save_msg_id(value, msgs_for_del=True)
+
+    await state.set_state(state=None)
+    await clbk.answer()
+
+    logger.debug(f'Exit')
