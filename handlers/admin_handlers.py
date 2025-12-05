f056fbbb34ab6d74539f26cf2c5836a38c98a454
nihosprop <niwibonihs@tutamail.com>
2025-12-05 23:00:44 +0300

    refactor

--- handlers/admin_handlers.py
+++ handlers/admin_handlers.py
@@ -29,15 +29,18 @@ async def cmd_start(
 
     await msg_processor.deletes_messages(msgs_for_del=True)
     await state.clear()
-    value = await msg.answer(LexiconRu.text_survey, reply_markup=kb_butt_quiz,
-                             disable_web_page_preview=True)
+    value = await msg.answer(
+        LexiconRu.text_survey, reply_markup=kb_butt_quiz,
+        disable_web_page_preview=True)
     await msg_processor.save_msg_id(value, msgs_for_del=True)
 
 
-@admin_router.callback_query(F.data == 'back',
-                             StateFilter(FSMAdminPanel.fill_newsletter))
-async def clbk_back_newsletter(clbk: CallbackQuery, state: FSMContext,
-                               redis_data: Redis):
+@admin_router.callback_query(
+    F.data == 'back',
+    StateFilter(FSMAdminPanel.fill_newsletter))
+async def clbk_back_newsletter(
+        clbk: CallbackQuery, state: FSMContext,
+        redis_data: Redis):
     end_cert = str(await redis_data.get('end_number')).zfill(6)
 
     await clbk.message.edit_text(
@@ -46,12 +49,14 @@ async def clbk_back_newsletter(clbk: CallbackQuery, state: FSMContext,
     await state.set_state(FSMAdminPanel.admin_menu)
     await clbk.answer()
 
+
 @admin_router.message(F.text == '/admin')
 async def cmd_admin(
         msg: Message, state: FSMContext, redis_data: Redis,
         msg_processor: MessageProcessor):
-    logger_admin.info(f'Вход в админку:{msg.from_user.id}'
-                      f':{await get_username(msg)}')
+    logger_admin.info(
+        f'Вход в админку:{msg.from_user.id}'
+        f':{await get_username(msg)}')
     keys = set(filter(lambda _id: _id.isdigit(), await redis_data.keys()))
     logger_admin.debug(f'{keys=}')
 
@@ -59,8 +64,9 @@ async def cmd_admin(
     await msg.delete()
     end_cert = str(await redis_data.get('end_number')).zfill(6)
 
-    value = await msg.answer(LexiconRu.text_adm_panel.format(end_cert=end_cert),
-                     reply_markup=kb_admin)
+    value = await msg.answer(
+        LexiconRu.text_adm_panel.format(end_cert=end_cert),
+        reply_markup=kb_admin)
     await msg_processor.save_msg_id(value, msgs_for_del=True)
     await state.set_state(FSMAdminPanel.admin_menu)
 
@@ -69,92 +75,108 @@ async def cmd_admin(
 async def cmd_exit(
         clbk: CallbackQuery, state: FSMContext,
         msg_processor: MessageProcessor):
-    logger_admin.info(f'Выход из админки:'
-                      f'{clbk.from_user.id}:{await get_username(clbk)}')
+    logger_admin.info(
+        f'Выход из админки:'
+        f'{clbk.from_user.id}:{await get_username(clbk)}')
     await state.set_state(state=None)
-    value = await clbk.message.edit_text(f'Вы вышли из админ-панели✅\n'
-                                         f'{LexiconRu.text_survey}',
-                                         reply_markup=kb_butt_quiz,
-                                         disable_web_page_preview=True)
+    value = await clbk.message.edit_text(
+        f'Вы вышли из админ-панели✅\n'
+        f'{LexiconRu.text_survey}',
+        reply_markup=kb_butt_quiz,
+        disable_web_page_preview=True)
     await msg_processor.save_msg_id(value, msgs_for_del=True)
     await clbk.answer()
 
-@admin_router.callback_query(F.data == 'certs_data',
-                             StateFilter(FSMAdminPanel.admin_menu))
-async def clbk_check_data_certs(clbk: CallbackQuery, state: FSMContext,
-                                redis_data: Redis):
-    logger_admin.info(f'Запрос данных по сертификатам:'
-                      f'{clbk.from_user.id}:{await get_username(clbk)}')
+
+@admin_router.callback_query(
+    F.data == 'certs_data',
+    StateFilter(FSMAdminPanel.admin_menu))
+async def clbk_check_data_certs(
+        clbk: CallbackQuery, state: FSMContext,
+        redis_data: Redis):
+    logger_admin.info(
+        f'Запрос данных по сертификатам:'
+        f'{clbk.from_user.id}:{await get_username(clbk)}')
     await clbk.answer('Сбор данных…')
     text = await get_data_users(clbk, redis_data=redis_data)
     await clbk.message.edit_text(text=text)
     await state.clear()
 
-@admin_router.callback_query(F.data == 'add_admin',
-                             StateFilter(FSMAdminPanel.admin_menu))
+
+@admin_router.callback_query(
+    F.data == 'add_admin',
+    StateFilter(FSMAdminPanel.admin_menu))
 async def clbk_make_cert(clbk: CallbackQuery):
     await clbk.answer('Копка в разработке', show_alert=True)
 
 
-@admin_router.callback_query(F.data == 'make_cert',
-                             StateFilter(FSMAdminPanel.admin_menu))
+@admin_router.callback_query(
+    F.data == 'make_cert',
+    StateFilter(FSMAdminPanel.admin_menu))
 async def clbk_make_cert(clbk: CallbackQuery):
     await clbk.answer('Копка в разработке', show_alert=True)
 
 
-@admin_router.callback_query(F.data == 'newsletter',
-                            StateFilter(FSMAdminPanel.admin_menu))
-async def clbk_newsletter(clbk: CallbackQuery, state: FSMContext,
-                          msg_processor: MessageProcessor):
+@admin_router.callback_query(
+    F.data == 'newsletter',
+    StateFilter(FSMAdminPanel.admin_menu))
+async def clbk_newsletter(
+        clbk: CallbackQuery, state: FSMContext,
+        msg_processor: MessageProcessor):
     # await clbk.answer(f'Кнопка в разработке', show_alert=True)
     value = await clbk.message.edit_text(
-            f'1. Отправить боту сообщение для рассылки.\n\n'
-                f'2. Подтвердить на следующем шаге!',
-                                 reply_markup=kb_back_cancel)
+        f'1. Отправить боту сообщение для рассылки.\n\n'
+        f'2. Подтвердить на следующем шаге!',
+        reply_markup=kb_back_cancel)
     await msg_processor.save_msg_id(value, msgs_for_del=True)
     await state.set_state(FSMAdminPanel.fill_newsletter)
     await clbk.answer()
 
 
 @admin_router.message(StateFilter(FSMAdminPanel.fill_newsletter))
-async def msg_for_newsletter(msg: Message, state: FSMContext,
-                             msg_processor: MessageProcessor):
+async def msg_for_newsletter(
+        msg: Message, state: FSMContext,
+        msg_processor: MessageProcessor):
     logger_admin.debug('Entry')
 
     await msg_processor.deletes_messages(msgs_for_del=True)
     msg_letter = msg.text
     await state.update_data(msg_letter=msg_letter)
-    value = await msg.answer('Проверьте сообщение.\n\n'
-                     'Подтвердите или отмените рассылку.\n'
-                     'После завершения придет инфо-сообщение о количестве'
-                             ' доставок.',
-                     reply_markup=kb_done_newsletter)
+    value = await msg.answer(
+        'Проверьте сообщение.\n\n'
+        'Подтвердите или отмените рассылку.\n'
+        'После завершения придет инфо-сообщение о количестве'
+        ' доставок.',
+        reply_markup=kb_done_newsletter)
     await state.update_data({'msg_del_on_key': str(value.message_id)})
     await state.set_state(FSMAdminPanel.fill_confirm_newsletter)
 
     logger_admin.debug('Exit')
 
 
-@admin_router.callback_query(F.data == 'done',
-                             StateFilter(FSMAdminPanel.fill_confirm_newsletter))
-async def clbk_done_newsletter(clbk: CallbackQuery,
-                               redis_data: Redis, redis_que: RedisSettings,
-                               state: FSMContext,
-                               msg_processor: MessageProcessor,
-                               config: Config):
+@admin_router.callback_query(
+    F.data == 'done',
+    StateFilter(FSMAdminPanel.fill_confirm_newsletter))
+async def clbk_done_newsletter(
+        clbk: CallbackQuery,
+        redis_data: Redis, redis_que: RedisSettings,
+        state: FSMContext,
+        msg_processor: MessageProcessor,
+        config: Config):
     logger_admin.debug('Entry')
 
     await msg_processor.delete_message()
 
     msg_letter = await state.get_value('msg_letter')
     user_ids = set(
-            map(int, filter(lambda _id: _id.isdigit(), await redis_data.keys())))
+        map(int, filter(lambda _id: _id.isdigit(), await redis_data.keys())))
     end_cert = str(await redis_data.get('end_number')).zfill(6)
     admin_ids: str = config.tg_bot.id_admins
     try:
-        await mass_mailing(redis_que=redis_que, user_ids=user_ids,
-                           message=msg_letter, admin_ids=admin_ids,
-                           end_cert=end_cert)
+        await mass_mailing(
+            redis_que=redis_que, user_ids=user_ids,
+            message=msg_letter, admin_ids=admin_ids,
+            end_cert=end_cert)
     except Exception as err:
         logger_admin.error(f'Ошибка при рассылке: {err}', exc_info=True)
     await state.set_state(FSMAdminPanel.admin_menu)
